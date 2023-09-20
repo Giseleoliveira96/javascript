7b67a15455f8cd5d76a5f53c3f80d09fd90a4437
Gisele Eduarda Lima de Oliveira <132854658+Giseleoliveira96@users.noreply.github.com>
2023-09-20 19:32:12 -0300

    aprendendo Array

---
+++ aula16/função01.js
@@ -0,0 +1,10 @@
+function parimpar (n){
+    if (n%2 ==0) {
+        return 'Par!'
+    } else{
+        return 'Ímpar!'
+    }
+}
+
+ //let res = parimpar(4), tem que colocar o console para aparecer
+ console.log(parimpar(133)) // Já vai direto sem precisar colocar VAR ou LET
